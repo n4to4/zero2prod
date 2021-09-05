4c5a095851acee26121df0021144767bac421db7
n4to4 <n4to4k@gmail.com>
2021-09-05 11:00:38 +0900

    postgres

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -2,7 +2,7 @@
 set -x
 set -eo pipefail
 
-if ! [ -x "$(command -v sqlx)"]; then
+if ! [ -x "$(command -v sqlx)" ]; then
   echo >&2 "Error: `sqlx` is not installed."
   echo >&2 "Use:"
   echo >&2 "    cargo install sqlx-cli --no-default-features --features postgres"
@@ -15,14 +15,17 @@ DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
 DB_NAME="${POSTGRES_DB:=newsletter}"
 DB_PORT="${POSTGRES_PORT:=5432}"
 
-docker run \
-  -e POSTGRES_USER=${DB_USER} \
-  -e POSTGRES_PASSWORD=${DB_PASSWORD} \
-  -e POSTGRES_DB=${DB_NAME} \
-  -p "${DB_PORT}":5432 \
-  --name postgres \
-  -d postgres \
-  postgres -N 1000
+if [[ -z "${SKIP_DOCKER}" ]]
+then
+  docker run \
+    -e POSTGRES_USER=${DB_USER} \
+    -e POSTGRES_PASSWORD=${DB_PASSWORD} \
+    -e POSTGRES_DB=${DB_NAME} \
+    -p "${DB_PORT}":5432 \
+    --name postgres \
+    -d postgres \
+    postgres -N 1000
+fi
 
 export PGPASSWORD="${DB_PASSWORD}"
 PSQL="docker exec -it postgres psql"
@@ -36,3 +39,6 @@ done
 
 export DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}
 sqlx database create
+sqlx migrate run
+
+>&2 echo "Postgres has been migrated, ready to go!"

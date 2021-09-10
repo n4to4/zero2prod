5222db07b4a6943f31d6b4dbf6e5a31be5c9e140
n4to4 <n4to4k@gmail.com>
2021-09-10 19:48:24 +0900

    logging

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -10,7 +10,19 @@ pub struct FormData {
 }
 
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
-    log::info!("Saving new subscriber details in the database");
+    println!("sub");
+
+    let request_id = Uuid::new_v4();
+    log::info!(
+        "request_id {} - Adding '{}' '{}' as a new subscriber.",
+        request_id,
+        form.email,
+        form.name
+    );
+    log::info!(
+        "request_id {} - Saving new subscriber details in the database",
+        request_id
+    );
     match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -21,15 +33,22 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
         form.name,
         Utc::now(),
     )
-    .execute(pool.get_ref())
+    .execute(pool.as_ref())
     .await
     {
         Ok(_) => {
-            log::info!("New subscriber details have been saved");
+            log::info!(
+                "request_id {} - New New subscriber details have been saved",
+                request_id
+            );
             HttpResponse::Ok().finish()
         }
         Err(e) => {
-            log::error!("Failed to execute query: {:?}", e);
+            log::error!(
+                "request_id {} - Failed to execute query: {:?}",
+                request_id,
+                e
+            );
             HttpResponse::InternalServerError().finish()
         }
     }

6509bed440b7fdaf8299f2c273a957382500a8d5
n4to4 <n4to4k@gmail.com>
2021-09-20 10:23:00 +0900

    config

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -44,7 +44,7 @@ async fn spawn_app() -> TestApp {
 }
 
 async fn configure_database(config: &DatabaseSettings) -> Pool<Postgres> {
-    let mut connection = PgConnection::connect(&config.connection_string_without_db())
+    let mut connection = PgConnection::connect_with(&config.without_db())
         .await
         .expect("Failed to connect to Postgres");
     connection
@@ -53,7 +53,7 @@ async fn configure_database(config: &DatabaseSettings) -> Pool<Postgres> {
         .expect("Failed to create database.");
 
     // Migrate database
-    let connection_pool = PgPool::connect(&config.connection_string())
+    let connection_pool = PgPool::connect_with(config.with_db())
         .await
         .expect("Failed to connect to Postgres.");
     sqlx::migrate!("./migrations")

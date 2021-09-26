c18e06a599461530c3d49d7ad47eef206d213031
n4to4 <n4to4k@gmail.com>
2021-09-26 14:08:12 +0900

    less allocations

--- src/email_client.rs
+++ src/email_client.rs
@@ -10,12 +10,12 @@ pub struct EmailClient {
 
 #[derive(Debug, serde::Serialize)]
 #[serde(rename_all = "PascalCase")]
-struct SendEmailRequest {
-    from: String,
-    to: String,
-    subject: String,
-    html_body: String,
-    text_body: String,
+struct SendEmailRequest<'a> {
+    from: &'a str,
+    to: &'a str,
+    subject: &'a str,
+    html_body: &'a str,
+    text_body: &'a str,
 }
 
 impl EmailClient {
@@ -37,11 +37,11 @@ impl EmailClient {
     ) -> Result<(), reqwest::Error> {
         let url = format!("{}/email", self.base_url);
         let request_body = SendEmailRequest {
-            from: self.sender.as_ref().to_owned(),
-            to: recipent.as_ref().to_owned(),
-            subject: subject.to_owned(),
-            html_body: html_content.to_owned(),
-            text_body: text_content.to_owned(),
+            from: self.sender.as_ref(),
+            to: recipent.as_ref(),
+            subject,
+            html_body: html_content,
+            text_body: text_content,
         };
         let _builder = self
             .http_client
